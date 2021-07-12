74d2b1374757058f6afed9203df0fceefea5dba7
Carlos Machado <carlosebmachado@outlook.com>
2021-07-12 13:20:54 -0300

    hamburger menu

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var doc = $('html, body');
+
+$('a').click(function () {
+  doc.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+  return false;
+});
+
+$('#hmenu').click(function () {
+  var menu = $('#nav-menu');
+  if (menu.is(":hidden")) {
+    menu.show();
+  } else {
+    menu.hide();
+  }
+});
+
+$('header nav a').click(function () {
+  $('#nav-menu').hide();
+});
+
+$(document).ready(function() {
+  $(window).trigger('resize');
+});
+
+$(window).resize(function() {
+  if (window.screen.width <= 540) {
+    $('#nav-menu').hide();
+    $('#hmenu').show();
+  } else {
+    $('#hmenu').hide();
+    $('#nav-menu').show();
+  }
+});
